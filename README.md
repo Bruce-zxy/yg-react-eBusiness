4f598fac71c413f2daafbd78d6f5946dd2d2c43a
bruce-zxy <bruce_zxy@163.com>
2017-11-21 19:40:24 +0800

    reset

--- README.md
+++ README.md
@@ -3,41 +3,56 @@
 
 ----------
 
-### 
 - **[项目目录结构](https://bruce-zxy.github.io/img/yg-react-eBusiness.png)**
 
-文字简介：
+文字简介（实时更新）：
 
 <pre>
 .
-├── build
+├── build						// 打包后生成的静态文件
 |	├── index.html
 |	├── bundle.js
 |	└── man.min.css
-├── dist
+├── dist						// 打包前准备的原始文件
 |	└── index.html
-├── data
+├── data						// 静态数据文件
 |	└── data.json
-└── src
-	├── component
-	|	├── template
-	|	|	├── template.js
-	|	|	├── template.style.js
-	|	|	├── index.js
+└── src							// React组件源
+	├── component				// React最小单元组件源
+	|	└── Foo
+	|		├── Foo.js
+	|		├── Foo.style.js
+	|		├── Index.js
+	|		├── img
+	|		└── css
+	├── app						// React动态组件模板
+	|	├── Foo
+	|	|	├── Foo.js
+	|	|	├── Foo.style.js
+	|	|	├── Index.js
 	|	|	├── img
 	|	|	└── css
 	|	└── App.js
-	├── views
-	|	└── template
-	|		└── view.js
-	├── vendor
+	├── views					// React静态组件模板
+	|	└── Foo
+	|		└── View.js
+	├── vendor					// 第三方库文件
 	|	└── jQuery-1.9.1
 	|		└── jQuery-1.9.1.min.js
-	└── store
-		└── template
-			└── template.store.js
+	└── store					// React数据流组件模板
+		└── Foo
+			└── Foo.store.js
 </pre>
 
-图片简介：
+图片简介（参考）：
 
 ![项目目录结构导图](https://bruce-zxy.github.io/img/yg-react-eBusiness.png)
+
+
+
+- **开发环境测试**
+
+	- cd yg-react-eBusiness
+    - npm install
+    - npm start
+    - 打开浏览器访问localhost:8888
--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,51 @@
+import React, { Component } from "react";
+import ReactDom from 'react-dom';
 
-'use strict';
-
-//引入样式文件
-import './styles/app.scss';
+import { Router, Route, hashHistory } from 'react-router';
 
 
-//引入组件
-import './components/App.js';
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <h5 className="title">hello, yeoman app!</h5>
+                <div>React Router: </div>
+                <div><a href="#/list">list page</a></div>
+                <div><a href="#/detail">detail page</a></div>
+            </div>
+        );
+    }
+};
+
+class List extends React.Component {
+    render() {
+        return (
+            <div>
+                <h5 className="title">hello, yeoman app!</h5>
+                <div><a href="#/">返回首页</a></div>
+                <div>这是列表页</div>
+            </div>
+        );
+    }
+};
+
+class Detail extends React.Component {
+    render() {
+        return (
+            <div>
+                <h5 className="title">hello, yeoman app!</h5>
+                <div><a href="#/">返回首页</a></div>
+                <div>这是详情页</div>
+            </div>
+        );
+    }
+};
+
+//最终渲染
+ReactDom.render((
+    <Router history={hashHistory}>
+        <Route path='/' component={App}></Route>
+        <Route path='/list' component={List} />
+        <Route path='/detail' component={Detail} />
+    </Router>
+), document.getElementById('app'));
